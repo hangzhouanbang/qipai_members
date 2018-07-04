65e004660e94b022e4c559ee064284910c9281a0
Administrator <Administrator@PC-20180515KRAG>
2018-07-04 17:23:39 +0800

    领取邮件奖励会员表金币积分添加并发送Kafka更新

--- src/main/java/com/anbang/qipai/members/web/controller/MemberMailReward.java
+++ src/main/java/com/anbang/qipai/members/web/controller/MemberMailReward.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.anbang.qipai.members.cqrs.c.domain.MemberNotFoundException;
 import com.anbang.qipai.members.cqrs.c.service.MemberGoldCmdService;
 import com.anbang.qipai.members.cqrs.c.service.MemberScoreCmdService;
+import com.anbang.qipai.members.cqrs.q.dbo.MemberDbo;
+import com.anbang.qipai.members.msg.service.MembersMsgService;
+import com.anbang.qipai.members.plan.service.MemberService;
 import com.anbang.qipai.members.web.vo.CommonVO;
 import com.dml.accounting.InsufficientBalanceException;
 
@@ -24,19 +27,29 @@ public class MemberMailReward {
 	@Autowired
 	private MemberScoreCmdService memberScoreCmdService;
 	
+	@Autowired
+	private MemberService memberService;
+	
+	@Autowired
+	private MembersMsgService membersMsgService;
+	
 	@RequestMapping("/mail_reward")
 	@ResponseBody
 	public CommonVO mail_Reward(String memberId,Integer number,Integer integral,Integer vipcard) throws InsufficientBalanceException, MemberNotFoundException {
 		if(memberId != null) {
+			MemberDbo memberDbo = memberService.findMemberById(memberId);
 			if(number != null) {
+				memberDbo.setGold(memberDbo.getGold()+number);
 				memberGoldCmdService.giveGoldToMember(memberId, number, "mail_reward", System.currentTimeMillis());
 			}
 			if(integral != null) {
+				memberDbo.setScore(memberDbo.getScore()+integral);
 				memberScoreCmdService.giveScoreToMember(memberId, integral, "mail_reward", System.currentTimeMillis());
 			}
 			if(vipcard != null) {
 				
 			}
+			membersMsgService.updateMember(memberDbo);
 		}
 		return new CommonVO(); 
 	}
