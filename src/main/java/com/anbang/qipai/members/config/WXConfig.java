01ed9b7e58cbd1c7d0507809bcc78d71123fade6
林少聪 < @PC-20180515PRDG>
2018-11-20 10:57:58 +0800

    bugfix

--- src/main/java/com/anbang/qipai/members/config/WXConfig.java
+++ src/main/java/com/anbang/qipai/members/config/WXConfig.java
@@ -1,10 +1,7 @@
 package com.anbang.qipai.members.config;
 
 public class WXConfig {
-	public static final String APPID = "wxb841e562b0100c95";
-	public static final String MCH_ID = "1507283991";
-	public static final String KEY = "lW43jdfOP05467835mbJdsdfg985ppqw";
-	public static final String NOTIFY_URL = "http://47.96.20.47:82/clubcard/wxnotify";
+
 	public static final String APP_TRADE_TYPE = "APP";
 	public static final String H5_TRADE_TYPE = "MWEB";
 	public static final String SIGN_TYPE = "MD5";
