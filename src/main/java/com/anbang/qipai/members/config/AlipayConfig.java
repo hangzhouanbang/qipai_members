5093aa8bce30df98227a2694d33430545bf13880
林少聪 < @PC-20180515PRDG>
2018-12-04 09:57:48 +0800

    bugfix

--- src/main/java/com/anbang/qipai/members/config/AlipayConfig.java
+++ src/main/java/com/anbang/qipai/members/config/AlipayConfig.java
@@ -2,26 +2,6 @@ package com.anbang.qipai.members.config;
 
 public class AlipayConfig {
 	public static final String SERVICE = "mobile.securitypay.pay";
-	public static final String PID = "2088421963544543";
-	public static final String PRIVATE_KEY = "MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAOpGNomVnRIYP7IT"
-			+ "byIdEmBCR5uHuCl2Jg4Gs5Ik7k/WWBnv85ImsC7BVfsX3f1l94104L/J3H6/QvM4"
-			+ "7nXpFxC7NwPCIHm6sLbNTwFMcC3i8jQzxKyA5L/qNgaL9D96yvSuTm4huAFfp/qR"
-			+ "e2CceNVANJnQ2nB4GgatQcArGkKZAgMBAAECgYArfG8BKPFn+3JPVsYpOeZAUe1C"
-			+ "1HV91L2JmvrYJNzLmwjtf5nhxiar9x1Dp5GASN4jts9FKm4wZMZDqYyx/FtYV80z"
-			+ "eRqqlywitFByjYC2bUvMSFajDQDpsSIrB0sjWzjbUv7cUeoxxi+jlziO43BF0oLl"
-			+ "g41rdCjIaLSm5XK2tQJBAPWf3dEtcVT8KgPPQG4wm8gU9FonQCbr/e1J9FpRCKl/"
-			+ "t2s+OaxEtILqd9T8CudMuWPSpMehIV1Ty8JDfc16hQsCQQD0K5uSFIUroTEsVuOH"
-			+ "VMNz8E2Wwolg8CLaOTxl/dDaFfcq5qLsYVBuhjY5Lgoji2lZbo/eQOJ8A3oqdecT"
-			+ "KFVrAkBUSD78//Lbjot8MymQpe1OgqI2LTG+KUxAmBfYxeWLA+AUVI3Fpu2p3nqw"
-			+ "AqxbIeCbeDRCq++e7poEVtRcJaZxAkB/pzjLU40X9Ur3Cmoj+42/1HdMBWK7WnBu"
-			+ "NJQ+IkeJbQhu1muBN5NMZUB9/nLwiFdImUQAB14hRdQd1Mw9OM4HAkEA5V13rWfZ"
-			+ "p2b1g+p3LaHok5PEbLd28KXg4Y1ifdGwq0PfGNmLgg8eqHpbkJnViN/1zonXycpx" + "vcTzb6VWKE6ujQ==";
-	public static final String ALI_PUBLIC_KEY = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCnxj/9qwVfgoUh/y2W89L6BkRA"
-			+ "FljhNhgPdyPuBV64bfQNN1PjbCzkIM6qRdKBoLPXmKKMiFYnkd6rAoprih3/PrQE"
-			+ "B/VsW8OoM8fxn67UDYuyBTqA23MML9q1+ilIZwBC2AQ2UBVOrFXfFl75p6/B5Ksi" + "NG9zpgmLCUYuLkxpLQIDAQAB";
-	public static final String CHARSET = "utf-8";
-	public static final String SIGN_TYPE = "RSA";
-	public static final String SELLER_ID = "baoanb@qq.com";
-	public static final String NOTIFY_URL = "http://47.96.20.47:82/clubcard/alipaynotify";
+
 	public static final String PAYMENT_TYPE = "1";
 }
