c85252cd5371ea6b0827f26994155a7b77371723
林少聪 < @PC-20180515PRDG>
2019-01-08 15:58:03 +0800

    pom修改

--- src/main/java/com/anbang/qipai/members/cqrs/c/domain/hongbao/MemberHongBaoAccountAlreadException.java
+++ src/main/java/com/anbang/qipai/members/cqrs/c/domain/hongbao/MemberHongBaoAccountAlreadException.java
@@ -1,22 +1,13 @@
 package com.anbang.qipai.members.cqrs.c.domain.hongbao;
 
+/**
+ * 该会员已有红包帐户
+ * 
+ * @author lsc
+ *
+ */
 public class MemberHongBaoAccountAlreadException extends Exception {
-    public MemberHongBaoAccountAlreadException() {
-    }
 
-    public MemberHongBaoAccountAlreadException(String message) {
-        super(message);
-    }
+	private static final long serialVersionUID = -6732277219731846454L;
 
-    public MemberHongBaoAccountAlreadException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public MemberHongBaoAccountAlreadException(Throwable cause) {
-        super(cause);
-    }
-
-    public MemberHongBaoAccountAlreadException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
 }
--- src/main/java/com/anbang/qipai/members/cqrs/c/domain/prize/LotteryTable.java
+++ src/main/java/com/anbang/qipai/members/cqrs/c/domain/prize/LotteryTable.java
@@ -1,117 +1,118 @@
 package com.anbang.qipai.members.cqrs.c.domain.prize;
 
-import io.swagger.models.auth.In;
-
-import java.util.*;
-
 import static com.anbang.qipai.members.cqrs.c.domain.sign.Constant.TOTAL_PROP_COUNT;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 public class LotteryTable {
 
-    private List<Lottery> lotterySet = new ArrayList<>();
+	private List<Lottery> lotterySet = new ArrayList<>();
 
-    private Random random = new Random();
+	private Random random = new Random();
 
-    private TreeMap<Integer, Lottery> firstLotteryTable = new TreeMap<>();
+	private TreeMap<Integer, Lottery> firstLotteryTable = new TreeMap<>();
 
-    private TreeMap<Integer, Lottery> lotteryTable = new TreeMap<>();
+	private TreeMap<Integer, Lottery> lotteryTable = new TreeMap<>();
 
-    private TreeMap<Integer, Lottery> firstOverstepLotteryTable = new TreeMap<>();
+	private TreeMap<Integer, Lottery> firstOverstepLotteryTable = new TreeMap<>();
 
-    private TreeMap<Integer, Lottery> overstepLotteryTable = new TreeMap<>();
+	private TreeMap<Integer, Lottery> overstepLotteryTable = new TreeMap<>();
 
+	private void check(List<Lottery> updateSet) {
+		int firstPropCount = 0;
+		int propCount = 0;
+		for (Lottery lottery : updateSet) {
+			System.out.println("prop:" + lottery.getProp() + ",firstProp:" + lottery.getFirstProp());
+			propCount = propCount + lottery.getProp();
+			firstPropCount = firstPropCount + lottery.getFirstProp();
+		}
+		if (!equalsToTenMillion(firstPropCount) || !equalsToTenMillion(propCount)) {
+			throw new RuntimeException("first prop count or prop count does not equal to " + TOTAL_PROP_COUNT);
+		}
+	}
 
-    private void check(List<Lottery> updateSet) {
-        int firstPropCount = 0;
-        int propCount = 0;
-        for (Lottery lottery : updateSet) {
-            System.out.println("prop:" + lottery.getProp() + ",firstProp:" + lottery.getFirstProp());
-            propCount = propCount + lottery.getProp();
-            firstPropCount = firstPropCount + lottery.getFirstProp();
-        }
-        if (!equalsToTenMillion(firstPropCount) || !equalsToTenMillion(propCount)) {
-            throw new RuntimeException("first prop count or prop count does not equal to " + TOTAL_PROP_COUNT);
-        }
-    }
+	private boolean equalsToTenMillion(int n) {
+		return n == TOTAL_PROP_COUNT;
+	}
 
-    private boolean equalsToTenMillion(int n) {
-        return n == TOTAL_PROP_COUNT;
-    }
+	public void updateLotterySet(List<Lottery> lotterySet) {
+		this.check(lotterySet);
+		this.clearBefore();
+		this.lotterySet = lotterySet;
+		int firstProp = 0;
+		int prop = 0;
+		int firstOverstepProp = 0;
+		int overstepProp = 0;
 
-    public void updateLotterySet(List<Lottery> lotterySet) {
-        this.check(lotterySet);
-        this.clearBefore();
-        this.lotterySet = lotterySet;
-        int firstProp = 0;
-        int prop = 0;
-        int firstOverstepProp = 0;
-        int overstepProp = 0;
+		for (Lottery lottery : lotterySet) {
+			prop += lottery.getProp();
+			if (lottery.getProp() != 0) {
+				lotteryTable.put(prop, lottery);
+			}
+			firstProp += lottery.getFirstProp();
+			if (lottery.getFirstProp() != 0) {
+				firstLotteryTable.put(firstProp, lottery);
+			}
+			if (lottery.isOverStep()) {
+				firstOverstepProp += lottery.getFirstProp();
+				overstepProp += lottery.getProp();
+				firstOverstepLotteryTable.put(firstOverstepProp, lottery);
+				overstepLotteryTable.put(overstepProp, lottery);
+			}
 
-        for (Lottery lottery : lotterySet) {
-            prop += lottery.getProp();
-            if (lottery.getProp() != 0) {
-                lotteryTable.put(prop, lottery);
-            }
-            firstProp += lottery.getFirstProp();
-            if (lottery.getFirstProp() != 0) {
-                firstLotteryTable.put(firstProp, lottery);
-            }
-            if (lottery.isOverStep()) {
-                firstOverstepProp += lottery.getFirstProp();
-                overstepProp += lottery.getProp();
-                firstOverstepLotteryTable.put(firstOverstepProp, lottery);
-                overstepLotteryTable.put(overstepProp, lottery);
-            }
+		}
+	}
 
-        }
-    }
+	private void clearBefore() {
+		this.lotterySet.clear();
+		this.lotteryTable.clear();
+		this.firstLotteryTable.clear();
+	}
 
-    private void clearBefore() {
-        this.lotterySet.clear();
-        this.lotteryTable.clear();
-        this.firstLotteryTable.clear();
-    }
+	public List<Lottery> list() {
+		return Collections.unmodifiableList(this.lotterySet);
+	}
 
-    public List<Lottery> list() {
-        return Collections.unmodifiableList(this.lotterySet);
-    }
+	public RaffleHistory raffle(String memberId, boolean first) throws StockInsufficientException {
+		int rand = random.nextInt(TOTAL_PROP_COUNT);
+		Lottery lottery = null;
+		RaffleHistory raffleHistory = null;
+		if (first) {
+			lottery = this.raffleConsiderofInsufficienStock(rand, firstLotteryTable, firstOverstepLotteryTable);
+			raffleHistory = new RaffleHistory(memberId, lottery, System.currentTimeMillis(), true);
+		} else {
 
-    public RaffleHistory raffle(String memberId, boolean first) throws StockInsufficientException {
-        int rand = random.nextInt(TOTAL_PROP_COUNT);
-        Lottery lottery = null;
-        RaffleHistory raffleHistory = null;
-        if (first) {
-            lottery = this.raffleConsiderofInsufficienStock(rand, firstLotteryTable, firstOverstepLotteryTable);
-            raffleHistory = new RaffleHistory(memberId, lottery, System.currentTimeMillis(), true);
-        } else {
+			lottery = this.raffleConsiderofInsufficienStock(rand, lotteryTable, overstepLotteryTable);
+			raffleHistory = new RaffleHistory(memberId, lottery, System.currentTimeMillis(), false);
+		}
+		return raffleHistory;
+	}
 
-            lottery = this.raffleConsiderofInsufficienStock(rand, lotteryTable, overstepLotteryTable);
-            raffleHistory = new RaffleHistory(memberId, lottery, System.currentTimeMillis(), false);
-        }
-        return raffleHistory;
-    }
+	private Lottery raffleInternal(Integer random, TreeMap<Integer, Lottery> table) throws StockInsufficientException {
+		final SortedMap<Integer, Lottery> sortedMap = table.tailMap(random, true);
+		Lottery lottery = null;
+		if (sortedMap.isEmpty()) {
+			lottery = table.firstEntry().getValue();
+		} else {
+			final Integer targetKey = sortedMap.firstKey();
+			lottery = sortedMap.get(targetKey);
+		}
+		lottery.descStock();
+		return lottery;
+	}
 
-    private Lottery raffleInternal(Integer random, TreeMap<Integer, Lottery> table) throws StockInsufficientException {
-        final SortedMap<Integer, Lottery> sortedMap = table.tailMap(random, true);
-        Lottery lottery = null;
-        if (sortedMap.isEmpty()) {
-            lottery = table.firstEntry().getValue();
-        } else {
-            final Integer targetKey = sortedMap.firstKey();
-            lottery = sortedMap.get(targetKey);
-        }
-        lottery.descStock();
-        return lottery;
-    }
-
-    private Lottery raffleConsiderofInsufficienStock(Integer random, TreeMap<Integer, Lottery> table,
-                                                     TreeMap<Integer, Lottery> overstepLotteryTable) throws StockInsufficientException {
-        try {
-            return this.raffleInternal(random, table);
-        } catch (StockInsufficientException e) {
-            return this.raffleInternal(random, overstepLotteryTable);
-        }
-    }
-
+	private Lottery raffleConsiderofInsufficienStock(Integer random, TreeMap<Integer, Lottery> table,
+			TreeMap<Integer, Lottery> overstepLotteryTable) throws StockInsufficientException {
+		try {
+			return this.raffleInternal(random, table);
+		} catch (StockInsufficientException e) {
+			return this.raffleInternal(random, overstepLotteryTable);
+		}
+	}
 
 }
