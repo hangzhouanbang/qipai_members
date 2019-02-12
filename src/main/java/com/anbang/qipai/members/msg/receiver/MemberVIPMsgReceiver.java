5d803bd17993ec34b7da92d108fc3a50b2eb0f06
林少聪 < @PC-20180515PRDG>
2019-02-12 09:52:37 +0800

    VIP漏洞

--- src/main/java/com/anbang/qipai/members/msg/receiver/MemberVIPMsgReceiver.java
+++ src/main/java/com/anbang/qipai/members/msg/receiver/MemberVIPMsgReceiver.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 
+import com.anbang.qipai.members.cqrs.q.dbo.MemberDbo;
 import com.anbang.qipai.members.cqrs.q.service.MemberAuthQueryService;
 import com.anbang.qipai.members.msg.channel.sink.MemberVIPSink;
 import com.anbang.qipai.members.msg.msjobj.CommonMO;
 import com.anbang.qipai.members.msg.service.MemberVIPRecordMsgService;
+import com.anbang.qipai.members.msg.service.MembersMsgService;
 import com.anbang.qipai.members.plan.bean.MemberVIPRecord;
 import com.anbang.qipai.members.plan.service.MemberVIPRecordService;
 import com.google.gson.Gson;
@@ -26,18 +28,22 @@ public class MemberVIPMsgReceiver {
 	@Autowired
 	private MemberAuthQueryService memberAuthQueryService;
 
+	@Autowired
+	private MembersMsgService membersMsgService;
+
 	private Gson gson = new Gson();
 
 	@StreamListener(MemberVIPSink.MEMBERVIPACCOUNTING)
-	public void memberScoreAccounting(CommonMO mo) {
+	public void memberVIP(CommonMO mo) {
 		String msg = mo.getMsg();
 		String json = gson.toJson(mo.getData());
-		if ("recharge vip".equals(msg)) {
+		if ("reward vip".equals(msg)) {
 			Map data = gson.fromJson(json, Map.class);
 			String memberId = (String) data.get("memberId");
-			long vipTime = ((Long) data.get("vipTime")).intValue();
+			long vipTime = ((Double) data.get("vipTime")).longValue();
 			String summary = (String) data.get("summary");
-			memberAuthQueryService.rechargeVip(memberId, vipTime);
+			MemberDbo member = memberAuthQueryService.rechargeVip(memberId, vipTime);
+			membersMsgService.rechargeVip(member);
 			MemberVIPRecord record = memberVIPRecordService.addMemberVIPRecord(memberId, vipTime, summary);
 			memberVIPRecordMsgService.record(record);
 		}
